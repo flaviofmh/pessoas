a4cbfe2352bcf0ab1bcce8bf2239cd533909550f
Flavio Honorato <flavio_fmh@hotmail.com>
2020-03-23 15:35:06 -0300

    Correção POST
    Foi adicionado o cors do spring e a inserção de foto no base64 e mapeamento do campo foto em string do tipo text

--- src/main/java/com/gerenciador/pessoas/PessoasApplication.java
+++ src/main/java/com/gerenciador/pessoas/PessoasApplication.java
@@ -4,6 +4,8 @@ import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class PessoasApplication {
@@ -16,5 +18,15 @@ public class PessoasApplication {
     public ModelMapper modelMapper() {
 	    return new ModelMapper();
     }
+
+    @Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/pessoas").allowedOrigins("http://localhost:4200");
+			}
+		};
+	}
 
 }
